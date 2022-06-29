3af08768346009a213c63e9e41e8a4217042b2a5
Daniel Garnica <thedaniel115@gmail.com>
2022-06-28 23:22:19 -0500

    Add UI Packages & Landpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Input, Text } from '@nextui-org/react';
+import { HiSearch } from 'react-icons/hi';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="bg-slate-50 h-screen flex justify-center flex-col items-center container">
+            <Text
+                h1
+                size={60}
+                css={{
+                    textGradient: '45deg, $blue600 -20%, $pink600 50%',
+                    textAlign: 'center',
+                }}
+                weight="bold"
+            >
+                News API - Client
+            </Text>
+
+            <div className="m-8 flex gap-2">
+                <Input clearable bordered labelPlaceholder="Search" />
+                <Button
+                    auto
+                    color="primary"
+                    className="bg-blue-500  text-white "
+                    icon={<HiSearch className="text-xl" />}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
